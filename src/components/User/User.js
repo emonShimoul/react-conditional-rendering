7eb64d250d04f73642c4b6102fb073bd8194dd78
emonShimoul <emonshimoul@gmail.com>
2022-05-28 11:12:34 +0600

    conditionar rendering using if else condition

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const User = (props) => {
+    const {familiar} = props;
+
+    // 1. Element variable
+    let greeting;
+    if(familiar){
+        greeting = <h5>Hello Bondhu How are you?</h5>
+    }
+    else{
+        greeting = <h5>Who the Hell are you? DGM.</h5>
+    }
+    
     return (
         <div>
             <div>
                 <h2>Greetings</h2>
+                {greeting}
+            </div>
+            <div>
+                <h2>Food</h2>
+            </div>
+            <div>
+                <h2>Profession</h2>
             </div>
         </div>
     );
