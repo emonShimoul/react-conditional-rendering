dda3d31bd3745737b383c5719bb65366a1c0ac6d
emonShimoul <emonshimoul@gmail.com>
2022-05-28 11:05:43 +0600

    added user component and change code in App.js

---
+++ src/components/User/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const User = (props) => {
+    return (
+        <div>
+            <div>
+                <h2>Greetings</h2>
+            </div>
+        </div>
+    );
+};
+
+export default User;
